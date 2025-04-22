f058ded355c9203503b0d197d5b8e81208f69b30
Nhat1911 <phamminhnhat19121103@gmail.com>
2025-04-22 12:46:21 +0700

    sua cac import cua cac file, them main.py để chạy chương trình

--- src/view/accountPage.py
+++ src/view/accountPage.py
@@ -1,45 +1,86 @@
 from tkinter import *
 from tkinter import ttk
 import json
+import os
+from src.Model.NhanVien import NhanVien
+
 
 class AccountPage:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
         self.root.columnconfigure(0, weight=1)
 
         def row_selection():
-            selected_item = accountTable.focus()
+            selected_item = self.accountTable.focus()
             if selected_item:
-                item_values = accountTable.item(selected_item)['values']
-                if item_values:                
-                    pass              
+                item_values = self.accountTable.item(selected_item)['values']
+                if item_values:
+                    pass
 
-        #Table
-        table_frame = Frame(self.root)        
-        table_frame.grid(row=0,column=0,sticky="news")        
+        # Table
+        self.table_frame = Frame(self.root)
+        self.table_frame.grid(row=0, column=0, sticky="news")
 
-        columns = ("#1", "#2", "#3","#4")
-        accountTable = ttk.Treeview(table_frame, columns=columns, show="headings")
-        accountTable.heading("#1",text="Tên tài khoản")
-        accountTable.heading("#2",text="Tên người dùng")
-        accountTable.heading("#3",text="Mật khẩu")
-        accountTable.heading("#4",text="Loại tài khoản")        
-        accountTable.column("#1", width=30,anchor="center")
-        accountTable.column("#2", width=50,anchor="center")
-        accountTable.column("#3", width=50,anchor="center")
-        accountTable.column("#4", width=200,anchor="center")
+        columns = ("#1", "#2", "#3", "#4")
+        self.accountTable = ttk.Treeview(
+            self.table_frame, columns=columns, show="headings")
+        self.accountTable.heading("#1", text="Tên tài khoản")
+        self.accountTable.heading("#2", text="Tên người dùng")
+        self.accountTable.heading("#3", text="Mật khẩu")
+        self.accountTable.heading("#4", text="Loại tài khoản")
+        self.accountTable.column("#1", width=30, anchor="center")
+        self.accountTable.column("#2", width=50, anchor="center")
+        self.accountTable.column("#3", width=50, anchor="center")
+        self.accountTable.column("#4", width=200, anchor="center")
 
-        accountTable.tag_configure("oddrow", background="#f0f0f0")
-        accountTable.tag_configure("evenrow", background="#ffffff")
+        self.accountTable.tag_configure("oddrow", background="#f0f0f0")
+        self.accountTable.tag_configure("evenrow", background="#ffffff")
 
-         # Thêm scrollbar dọc
-        scrollbar = ttk.Scrollbar(table_frame, orient="vertical", command=accountTable.yview)
-        accountTable.configure(yscroll=scrollbar.set)        
-        scrollbar.pack(side="right", fill="y")
-        accountTable.pack(fill="both",expand=TRUE)
-        with open("src/database/account.json","r",encoding="utf-8") as dataFile:
-            data = json.load(dataFile)
-            for index, item in enumerate(data):
-                tag = "evenrow" if index % 2 == 0 else "oddrow"                
-                accountTable.insert("",END,values=(item["tenTaiKhoan"],item["tenTaiKhoan"],item["matKhau"],item["loaiTaiKhoan"]),tags=(tag,))
-        accountTable.bind("<<TreeviewSelect>>",lambda e:row_selection())
+        # Thêm scrollbar dọc
+        self.scrollbar = ttk.Scrollbar(
+            self.table_frame, orient="vertical", command=self.accountTable.yview)
+        self.accountTable.configure(yscroll=self.scrollbar.set)
+        self.scrollbar.pack(side="right", fill="y")
+        self.accountTable.pack(fill="both", expand=TRUE)
+
+        filename = "src/database/account.json"
+        self.data = []
+        if os.path.exists(filename):
+            try:
+                with open(filename, "r", encoding="utf-8") as dataFile:
+                    self.data = json.load(dataFile)
+                    for index, item in enumerate(self.data):
+                        tag = "evenrow" if index % 2 == 0 else "oddrow"
+                        self.accountTable.insert("", END, values=(
+                            item["tenTaiKhoan"], item["tenNguoiDung"], item["matKhau"], item["loaiTaiKhoan"]), tags=(tag,))
+            except (json.JSONDecoder, FileNotFoundError):
+                self.data = []
+                admin = NhanVien("admin", "1234", "admin")
+                self.data.append(admin.to_dict())
+
+                # Tạo folder nếu chưa có
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+                with open(filename, "w", encoding="utf-8") as file:
+                    json.dump(self.data, file, ensure_ascii=False, indent=4)
+
+                for index, item in enumerate(self.data):
+                    tag = "evenrow" if index % 2 == 0 else "oddrow"
+                    self.accountTable.insert("", END, values=(
+                        item["tenTaiKhoan"], item["tenNguoiDung"], item["matKhau"], item["loaiTaiKhoan"]), tags=(tag,))
+        else:
+            self.data = []
+            admin = NhanVien("admin", "1234", "admin")
+            self.data.append(admin.to_dict())
+
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+            with open(filename, "w", encoding="utf-8") as file:
+                json.dump(self.data, file, ensure_ascii=False, indent=4)
+
+            for index, item in enumerate(self.data):
+                tag = "evenrow" if index % 2 == 0 else "oddrow"
+                self.accountTable.insert("", END, values=(
+                    item["tenTaiKhoan"], item["tenNguoiDung"], item["matKhau"], item["loaiTaiKhoan"]), tags=(tag,))
+
+        self.accountTable.bind("<<TreeviewSelect>>", lambda e: row_selection())
--- src/view/loginPage.py
+++ src/view/loginPage.py
@@ -1,83 +1,91 @@
 from tkinter import *
 from tkinter import messagebox
 import subprocess
-import mainPage
+from src.view.mainPage import MainPage as mainPage
 import json
-win = Tk()
-win.title("Login")
-icon = PhotoImage(file="src/image/warehouse.png")
-win.iconphoto(True,icon)
-win.geometry('925x500')
-win.configure(bg="#fff")
-win.resizable(False,False)
 
-img = PhotoImage(file="src/image/login.png")
-Label(win,image=img,bg='white').place(x=50,y=50)
 
-frame = Frame(win,width=350,height=350,bg="white")
-frame.place(x=480,y=70)
+def start_login():
+    win = Tk()
+    win.title("Login")
+    icon = PhotoImage(file="src/image/warehouse.png")
+    win.iconphoto(True, icon)
+    win.geometry('925x500')
+    win.configure(bg="#fff")
+    win.resizable(False, False)
 
-heading = Label(frame,text="Sign in",fg="#57a1f8",bg="white",font=('Microsoft YaHei UI Light',23,'bold'))
-heading.place(x=100,y=5)
-#----------------------------------------------------------------
-def on_enter(event):
-    widget = event.widget
-    widget.delete(0,END)
+    img = PhotoImage(file="src/image/login.png")
+    Label(win, image=img, bg='white').place(x=50, y=50)
 
-def on_leave(event):
-    name = user.get()
-    if name == "":
-        user.insert(0,"Username")
+    frame = Frame(win, width=350, height=350, bg="white")
+    frame.place(x=480, y=70)
 
-user = Entry(frame,width=25,fg="black",bg="white",font=('Microsoft YaHei UI Light',11),border=0)
-user.place(x=30,y=80)
-user.insert(0,"Username")
-user.bind("<FocusIn>",on_enter)
-user.bind("<FocusOut>",on_leave)
-Frame(frame,width=295,height=2,bg="black").place(x=25,y=107)
+    heading = Label(frame, text="Sign in", fg="#57a1f8",
+                    bg="white", font=('Microsoft YaHei UI Light', 23, 'bold'))
+    heading.place(x=100, y=5)
+    # ----------------------------------------------------------------
 
-#----------------------------------------------------------------
-def on_leave(event):
-    name = passwordBox.get()
-    if name == "":
-        passwordBox.insert(0,"Password")
+    def on_enter(event):
+        widget = event.widget
+        widget.delete(0, END)
 
-passwordBox = Entry(frame,width=25,fg="black",bg="white",font=('Microsoft YaHei UI Light',11),border=0)
-passwordBox.place(x=30,y=150)
-passwordBox.insert(0,"Password")
-passwordBox.bind("<FocusIn>",on_enter)
-passwordBox.bind("<FocusOut>",on_leave)
-Frame(frame,width=295,height=2,bg="black").place(x=25,y=177)
-#----------------------------------------------------------------
+    def on_leave(event):
+        name = user.get()
+        if name == "":
+            user.insert(0, "Username")
 
-def signIn():
-    username = user.get()
-    password = passwordBox.get()
-    with open("src/database/account.json","r") as open_file:
-        data = json.load(open_file)
-        flag = 0
-        for line in data:
-            if line["tenTaiKhoan"] == username and line["matKhau"] == password:
-                win.destroy()
-                mainPage.MainPage()  # Open Home Page
-                flag = 1
-                break
-        if flag == 0:
-            messagebox.showerror("Error","Invalid credentials!")
-        # if data["name"] == username and data["password"] == password:
-        #     win.destroy()
-        #     homePage.HomePage()  # Open Home Page            
-        # else:
-        #     messagebox.showerror("Error","Invalid credentials!")
-        # if username == "admin" and password == "1234":
-        #     #subprocess.Popen(["python", "homePage.py"])
-        #     win.destroy()
-        #     homePage.HomePage()  # Open Home Page                
-        # else:
-        #     messagebox.showerror("Error","Invalid credentials!")
+    user = Entry(frame, width=25, fg="black", bg="white",
+                 font=('Microsoft YaHei UI Light', 11), border=0)
+    user.place(x=30, y=80)
+    user.insert(0, "Username")
+    user.bind("<FocusIn>", on_enter)
+    user.bind("<FocusOut>", on_leave)
+    Frame(frame, width=295, height=2, bg="black").place(x=25, y=107)
 
-signInBtn = Button(frame,width=39,pady=7,text="Sign in",bg="#57a1f8",fg="white",border=0,command=signIn,default='active')
-signInBtn.place(x=35,y=204)
-signInBtn.bind('<Return>',lambda e:signIn())
+    # ----------------------------------------------------------------
+    def on_leave(event):
+        name = passwordBox.get()
+        if name == "":
+            passwordBox.insert(0, "Password")
 
-win.mainloop()
+    passwordBox = Entry(frame, width=25, fg="black", show="*", bg="white", font=(
+        'Microsoft YaHei UI Light', 11), border=0)
+    passwordBox.place(x=30, y=150)
+    passwordBox.insert(0, "Password")
+    passwordBox.bind("<FocusIn>", on_enter)
+    passwordBox.bind("<FocusOut>", on_leave)
+    Frame(frame, width=295, height=2, bg="black").place(x=25, y=177)
+    # ----------------------------------------------------------------
+
+    def signIn():
+        username = user.get()
+        password = passwordBox.get()
+        with open("src/database/account.json", "r") as open_file:
+            data = json.load(open_file)
+            flag = 0
+            for line in data:
+                if line["tenTaiKhoan"] == username and line["matKhau"] == password:
+                    win.destroy()
+                    mainPage()  # Open Home Page
+                    flag = 1
+                    break
+            if flag == 0:
+                messagebox.showerror("Error", "Invalid credentials!")
+            # if data["name"] == username and data["password"] == password:
+            #     win.destroy()
+            #     homePage.HomePage()  # Open Home Page
+            # else:
+            #     messagebox.showerror("Error","Invalid credentials!")
+            # if username == "admin" and password == "1234":
+            #     #subprocess.Popen(["python", "homePage.py"])
+            #     win.destroy()
+            #     homePage.HomePage()  # Open Home Page
+            # else:
+            #     messagebox.showerror("Error","Invalid credentials!")
+
+    signInBtn = Button(frame, width=39, pady=7, text="Sign in", bg="#57a1f8",
+                       fg="white", border=0, command=signIn, default='active')
+    signInBtn.place(x=35, y=204)
+    signInBtn.bind('<Return>', lambda e: signIn())
+
+    win.mainloop()

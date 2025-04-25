3c5c408db7859f34d61ef0f391a08a0a054a43ba
Nhat1911 <phamminhnhat19121103@gmail.com>
2025-04-25 15:24:21 +0700

    Sửa geometry bên loginPage và mainPage, thêm 3 frame bên accountPage

--- src/view/accountPage.py
+++ src/view/accountPage.py
@@ -10,6 +10,11 @@ class AccountPage:
         self.root = root
         self.root.columnconfigure(0, weight=1)
 
+        self.create_frame_account_table()
+        self.create_frame_button()
+        self.create_entry()
+
+    def create_frame_account_table(self):
         def row_selection():
             selected_item = self.accountTable.focus()
             if selected_item:
@@ -31,7 +36,7 @@ class AccountPage:
         self.accountTable.column("#1", width=30, anchor="center")
         self.accountTable.column("#2", width=50, anchor="center")
         self.accountTable.column("#3", width=50, anchor="center")
-        self.accountTable.column("#4", width=200, anchor="center")
+        self.accountTable.column("#4", width=50, anchor="center")
 
         self.accountTable.tag_configure("oddrow", background="#f0f0f0")
         self.accountTable.tag_configure("evenrow", background="#ffffff")
@@ -52,6 +57,84 @@ class AccountPage:
                 item["tenTaiKhoan"], item["tenNguoiDung"], item["matKhau"], item["loaiTaiKhoan"]), tags=(tag,))
 
         self.accountTable.bind("<<TreeviewSelect>>", lambda e: row_selection())
+
+    def create_frame_button(self):
+        self.frame_buttons = Frame(self.root)
+        self.frame_buttons.grid(row=1, column=0, sticky="news", pady=10)
+
+        self.btn_add = Button(self.frame_buttons, text="Thêm",
+                              width=10, height=2, font=("Arial", 13), bg="blue", fg="white")
+        self.btn_add.grid(row=0, column=0, padx=10)
+
+        self.btn_remove = Button(self.frame_buttons, text="Xóa",
+                                 width=10, height=2, font=("Arial", 13), bg="red", fg="white")
+        self.btn_remove.grid(row=0, column=1, padx=10)
+
+        self.btn_edit = Button(self.frame_buttons, text="Sửa",
+                               width=10, height=2, font=("Arial", 13), bg="green", fg="white")
+        self.btn_edit.grid(row=0, column=2, padx=10)
+
+    def create_entry(self):
+        self.frame_entries = Frame(self.root)
+        self.frame_entries.grid(row=2, column=0, sticky="news", pady=10)
+
+        # Username
+        self.lbl_username = Label(
+            self.frame_entries, text="Tên tài khoản: ", width=15, font=("Arial", 13))
+        self.lbl_username.grid(row=0, column=0, padx=10, pady=10)
+        self.entry_username = Entry(
+            self.frame_entries,
+            width=20,
+            font=("Arial", 13),
+            bd=1,
+            relief="solid",
+            highlightbackground="gray",
+            highlightthickness=2,
+            highlightcolor="blue")
+        self.entry_username.grid(row=0, column=1, padx=10, pady=10)
+
+        # Tên người dùng
+        self.lbl_fullname = Label(
+            self.frame_entries, text="Tên người dùng: ", width=15, font=("Arial", 13))
+        self.lbl_fullname.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_fullname = Entry(
+            self.frame_entries,
+            width=20,
+            font=("Arial", 13),
+            bd=1,
+            relief="solid",
+            highlightbackground="gray",
+            highlightthickness=2,
+            highlightcolor="blue")
+        self.entry_fullname.grid(row=1, column=1, padx=10, pady=10)
+
+        # Mật khẩu
+        self.lbl_password = Label(
+            self.frame_entries, text="Mật khẩu: ", width=15, font=("Arial", 13))
+        self.lbl_password.grid(row=2, column=0, padx=10, pady=10)
+        self.entry_password = Entry(
+            self.frame_entries,
+            width=20,
+            font=("Arial", 13),
+            bd=1,
+            relief="solid",
+            highlightbackground="gray",
+            highlightthickness=2,
+            highlightcolor="blue")
+        self.entry_password.grid(row=2, column=1, padx=10, pady=10)
+
+        # Role
+        self.lbl_role = Label(
+            self.frame_entries, text="Loại tài khoản: ", width=15, font=("Arial", 13))
+        self.lbl_role.grid(row=3, column=0, padx=10, pady=10)
+        self.entry_role = ttk.Combobox(
+            self.frame_entries,
+            width=20,
+            font=("Arial", 13),
+            state="readonly",  # ko cho người dùng gõ vào
+            values=["admin", "user"])
+        self.entry_role.current(1)  # mặc định là user
+        self.entry_role.grid(row=3, column=1, padx=10, pady=10)
 
     def load_data_from_file(self, filename):
         if not os.path.exists(filename):
